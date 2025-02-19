d32ce02cfee52b61505a547dd95eb0348977bbce
Luana Lima <luana.lima@sou.inteli.edu.br>
2025-02-19 07:47:38 -0300

    v2

--- src/initialScene.js
+++ src/initialScene.js
@@ -3,13 +3,20 @@ class InitialScene extends Phaser.Scene {
         super({ key: 'InitialScene' });
     }
 
+    
     preload() {
+        //Carregando as imagens que serão usadas na tela de início do jogo.
         this.load.image('backgroundInitialScene', 'assets/backgroundInitialScene.jpg');
+        this.load.image('startButton', 'assets/startButton.png');
     }
 
+
     create() {
+        //Altura e largura do jogo.
         var gameWidth = this.scale.width;
         var gameHeight = this.scale.height;    
+
+        //Criando o background do jogo: ajustando largura e altura.
         var initialSceneBackground = this.add.image(gameWidth/2, gameHeight/2, 'backgroundInitialScene');
         initialSceneBackground.setScale(gameWidth/initialSceneBackground.width, gameHeight/initialSceneBackground.height);
         this.add.text(gameWidth / 2, gameHeight / 2, "Aventura na floresta", {
@@ -18,6 +25,12 @@ class InitialScene extends Phaser.Scene {
             fontFamily: "Arial",
             align: "center"
         }).setOrigin(0.5);
+
+        //Criando o botão de start (início do jogo)
+        var startButton = this.add.image(gameWidth/2, gameHeight/1.5, 'startButton').setScale(0.4).setInteractive();
+        startButton.on('pointerdown', () =>{
+            this.scene.start('InitialScene');
+        })
     }
 }
 
