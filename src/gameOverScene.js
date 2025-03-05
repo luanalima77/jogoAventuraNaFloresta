5382ea7b7c5fc25c33ece9594eff88edd0507142
Luana Lima <luana.lima@sou.inteli.edu.br>
2025-03-05 12:51:18 -0300

    jogoFloresta

--- src/gameOverScene.js
+++ src/gameOverScene.js
@@ -27,13 +27,13 @@ class GameOverScene extends Phaser.Scene{
         this.background.setDisplaySize(gameWidth, gameHeight);
 
         //Mensagem de derrota.
-        this.defeatMessage = this.add.image(gameWidth/2, gameHeight/2, 'defeatMessage');
+        this.defeatMessage = this.add.image(gameWidth/2, gameHeight/2.3, 'defeatMessage');
 
         //Mostrando quantas moedas o jogador coletou.
-        this.add.text(gameWidth / 2, gameHeight / 1.35, `Moedas coletadas: ${data.points}`, {fontSize: "25px",fontFamily: "Poppins", color: "#ffffff"}).setOrigin(0.5);
+        this.add.text(gameWidth / 2, gameHeight / 1.4, `Moedas coletadas: ${data.points}`, {fontSize: "25px",fontFamily: "Poppins", color: "#ffffff"}).setOrigin(0.5);
         
         //Botão de reiniciar da tela de derrota
-        var restartButton = this.add.image(gameWidth / 2, gameHeight/1.2, 'playAgainButton').setScale(0.7).setInteractive();
+        var restartButton = this.add.image(gameWidth / 2, gameHeight/1.35, 'playAgainButton').setScale(0.7).setInteractive();
 
         //Quando o botão de reiniciar é clicado, o jogador pode voltar ao jogo.
         restartButton.on("pointerdown", () => {
